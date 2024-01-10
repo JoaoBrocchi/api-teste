2c83390e0fb96e64a78ba495d2d8ddcf35b38657
joaoBrocchi <joaobrocchiprofissional@gmail.com>
2024-01-10 14:29:29 -0300

    w

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,7 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE, "root", process.env.PASSWORD, {
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USUARIO, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'mysql', 
   });
